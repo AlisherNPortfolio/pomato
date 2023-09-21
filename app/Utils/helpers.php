e7162b2210637a10dca777c4826e84ffb2c23a76
Alisher <myemail9445@inbox.ru>
2023-09-21 17:24:19 +0500

    helpers

---
+++ app/Utils/helpers.php
@@ -0,0 +1,57 @@
+<?php
+
+if (!function_exists('is_prod')) {
+    /**
+     * Checks if application in production mode.
+     *
+     * @return bool
+     */
+    function is_prod()
+    {
+        return app()->environment() !== 'local';
+    }
+}
+
+if (!function_exists('success_response')) {
+    /**
+     * Json data wrapper for a success response to clients. Aimed for API applications. Returns Illuminate\Http\JsonResponse data wrapped with some meta data.
+     *
+     * @param [type] $data
+     * @param int    $code
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    function success_response($data, $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $data,
+        ], $code);
+    }
+}
+
+if (!function_exists('error_response')) {
+    /**
+     * Json data wrapper for a error response to clients. Aimed for API applications. Returns Illuminate\Http\JsonResponse data wrapped with some meta data.
+     *
+     * @param string $message
+     * @param int    $code
+     * @param [type] $details
+     *
+     * @return Illuminate\Http\JsonResponse
+     */
+    function error_response($message = '', $code = 0, $details = null)
+    {
+        $response = [
+            'success' => false,
+            'code' => $code,
+            'message' => $message,
+        ];
+
+        if (!is_prod()) {
+            $response['details'] = $details;
+        }
+
+        return response()->json($response);
+    }
+}
